b59610d0f78a8f81ee543a54298f6d44374ae50b
AlexBalew <balew48@gmail.com>
2023-02-18 19:14:17 +0300

    db & article types

---
+++ src/entities/Article/model/types/article.ts
@@ -0,0 +1,40 @@
+export const enum ArticleType {
+    Comics = 'Comics',
+    News = 'News',
+    Music = 'Music',
+    Movies = 'Movies',
+}
+
+export const enum ArticleBlockType {
+    Text = 'Text',
+    Image = 'Image'
+}
+
+export interface IArticleBlockBase {
+    id: string;
+    title?: string;
+    type: ArticleBlockType;
+}
+
+export interface IArticleBlockText extends IArticleBlockBase {
+    type: ArticleBlockType.Text
+    paragraphs?: string[];
+}
+
+export interface IArticleBlockImage extends IArticleBlockBase {
+    type: ArticleBlockType.Image
+    src: string;
+}
+
+export type IArticleBlock = IArticleBlockText | IArticleBlockImage;
+
+export interface IArticle {
+    id: string;
+    title?: string;
+    subtitle?: string;
+    img?: string;
+    views?: number;
+    createdAt?: string;
+    type: ArticleType[];
+    blocks?: IArticleBlock;
+}
